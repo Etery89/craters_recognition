413ee21d948f5072e54b071d5dc30ced888a3dfe
Natalya Komarova <shadowx110@yandex.ru>
2020-04-01 00:04:09 +0300

    Работа над функцией обработки параметров в блоке try/except

--- craters_recognition_interface.py
+++ craters_recognition_interface.py
@@ -13,7 +13,7 @@ class MyWidget(QtWidgets.QWidget):
         super().__init__()
 
         # QHBoxLayout 'open file"
-        self.file_open_button = QtWidgets.QPushButton('Open image')
+        self.file_open_button = QtWidgets.QPushButton('Open TIFF-file')
         self.file_open_lineedit = QtWidgets.QLineEdit()
 
 
@@ -73,6 +73,12 @@ class MyWidget(QtWidgets.QWidget):
         self.parameters_layout.addWidget(self.max_search_radius_qlable, 2, 2)
         self.parameters_layout.addWidget(self.max_search_radius_qlineedit, 2, 3)
 
+        # Program message output field
+        # self.program_message_label = QtWidgets.QLabel('Program messages')
+        self.program_message_field = QtWidgets.QTextEdit('Program messages')
+        self.program_message_field.setFixedSize(500, 100)
+        
+
         # QVBoxLayout "left parth"/"function parth"
 
         self.layout_function_parth =  QtWidgets.QVBoxLayout()
@@ -82,6 +88,7 @@ class MyWidget(QtWidgets.QWidget):
         self.layout_function_parth.addWidget(self.algorithm_button)
         self.layout_function_parth.addLayout(self.layout_open_shp_file)
         self.layout_function_parth.addWidget(self.additional_parameters_button)
+        self.layout_function_parth.addWidget(self.program_message_field)
 
         # QHBoxLayout "function part + image parth"
         self.image_label = QtWidgets.QLabel()
@@ -89,7 +96,7 @@ class MyWidget(QtWidgets.QWidget):
         self.image_label.setFrameShape(QtWidgets.QFrame.Box)
         self.image_label.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.image_label.setLineWidth(4)
-        self.image_label.setFrameRect(QtCore.QRect(QtCore.QPoint(0, 0), QtCore.QSize(500, 430)))
+        self.image_label.setFrameRect(QtCore.QRect(QtCore.QPoint(0, 0), QtCore.QSize(500, 470)))
 
         self.function_with_image_layout = QtWidgets.QHBoxLayout()
         self.function_with_image_layout.addLayout(self.layout_function_parth)
@@ -114,8 +121,9 @@ class MyWidget(QtWidgets.QWidget):
              'Images (*.jpg *.png *.bmp *.TIF *.TIFF)'
          )
         path_to_file_text = self.file_open_lineedit.setText(path_to_file)
+        # Вызов функции создания мозаики на основе открытого TIFF-файла (переписать код ниже, чтобы в изображении окрывалась мозаика)
         file_image = QtGui.QPixmap(path_to_file)
-        image_to_image_lable = self.image_label.setPixmap(file_image.scaled(500, 900, QtCore.Qt.KeepAspectRatio))
+        image_to_image_lable = self.image_label.setPixmap(file_image.scaled(600, 800, QtCore.Qt.KeepAspectRatio))
 
     @Slot()
     def open_shp_file(self):
@@ -129,15 +137,33 @@ class MyWidget(QtWidgets.QWidget):
 
     @Slot()
     def use_initial_data(self):
-        call_initial_data = initial_data("APOLLO17_DTM_150CM_180_45.tif", "APOLLO17_DTM_150CM.tiff", "crat_circle.shp")
+        file_open_text = self.file_open_lineedit.text()
+        print(file_open_text)
+        # call_initial_data = initial_data("APOLLO17_DTM_150CM_180_45.tif", "APOLLO17_DTM_150CM.tiff", "crat_circle.shp")
         # call_file_shp_generation_and_saving = file_shp_generation_and_saving()
         # demonstration_image = QtGui.QPixmap(call_file_shp_generation_and_saving)
         # demonstration_image_to_label = self.image_label.setPixmap(demonstration_image.scaled(500, 900, QtCore.Qt.KeepAspectRatio))
 
 
     @Slot()
-    def take_parameters(self):
+    def take_parameters_and_use_initial_data(self):
+        var_with_image_text = int(self.var_with_image_qlineedit.text())
+        min_distance_centers_text = int(self.min_distance_centers_qle.text())
+        parametr1_text = int(self.parametr1_qlineedit.text())
+        parametr2_text = int(self.parametr2_qlineedit.text())
+        min_search_radius_text = int(self.min_search_radius_qlineedit.text())
+        max_search_radius_text = int(self.max_search_radius_qlineedit.text())
+        file_tiff_open_text = self.file_open_lineedit.text()
+        # вызов функции генерации пустого шейп-файла, для последующей передачи его в функцию основного алгоритма
+        call_initial_data = initial_data("APOLLO17_DTM_150CM_180_45.tif", file_tiff_open_text, "crat_circle.shp", var_with_image_text, min_distance_centers_text, parametr1_text, parametr2_text, min_search_radius_text, max_search_radius_text)
         pass
+
+    def parameters_except(self, parameter):
+        try:
+            parameter_text = int(self.parameter.text())
+        except TypeError:
+            self.program_message_field.setText(f'Неправильный формат ввода. Вместо {parameter_text} введите целое число')
+        
         
 
 
