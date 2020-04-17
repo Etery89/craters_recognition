b16d97fbe7727a706109a021874e7010582af171
Zavyalov <zse117@yandex.ru>
2020-04-17 12:59:13 +0300

    name refactoring

--- simple_cv.py
+++ simple_cv.py
@@ -20,8 +20,8 @@ from numpy import uint8
 
 
 
-    
-def create_mosaic_file_path(dtm_input):
+    # create_mosaic_file_path --> default_mosaic_filename
+def default_mosaic_filename(dtm_input):
     mosaic_file_name = dtm_input.split('.')[0] + '_mosaic.tif'
     return mosaic_file_name
 
@@ -37,8 +37,10 @@ def hillshade(array, azimuth, angle_altitude):
     + cos(altituderad) * cos(slope)\
     * cos(azimuthrad - aspect)
     return 255*(shaded + 1)/2
+
 # функция создает мозаику с использованием исходного геофайла
-def create_mosaic(DTM_input, mosaic_file_name):
+# create_mosaic --> create_stored_mosaic 
+def create_stored_mosaic(DTM_input, mosaic_file_name):
     dtm = gdal.Open(DTM_input)
     dtm_prj = dtm.GetProjection()
     band = dtm.GetRasterBand(1)
@@ -54,17 +56,17 @@ def create_mosaic(DTM_input, mosaic_file_name):
     mosaic_dataset.GetRasterBand(1).WriteArray(hs_array)
     mosaic_dataset = None
 
-# create_mosaic(dtm_input, create_mosaic_file_path(dtm_input))
 
 
    
 
-    #создание shp файла
-def create_shp(shp_name):
+    # создание shp файла
+    # create_shp --> store_shape
+def create_stored_shp(shp_name):
     driverName = "ESRI Shapefile"
     drv = ogr.GetDriverByName(driverName)
     ogrData = drv.CreateDataSource(shp_name)
-
+    #открываем dtm и берем оттуда параметры проекции
     dtm = gdal.Open(dtm_input)
     dtm_prj = dtm.GetProjection()
     srs = osr.SpatialReference(wkt=dtm_prj)
@@ -80,31 +82,29 @@ def create_shp(shp_name):
     crat_layer.CreateField(fieldDiam)
     crat_layer.CreateField(ogr.FieldDefn("Latitude", ogr.OFTReal))
     crat_layer.CreateField(ogr.FieldDefn("Longitude", ogr.OFTReal))
-    return shp_name
-# create_shp()
 
-# def first_button(mosaic):
 
-def image_create(mosaic_file_path):
+
+# image_create --> get_colorized_image
+def get_colorized_image(mosaic_file_path):
     #открытие исходной мозаики
     # mosaic = dtm_input.split('.')[0] + '_mosaic.tif'
     image = cv2.imread(mosaic_file_path, 0)
-    #cv2.imshow("Image", image)
     image = cv2.GaussianBlur(image, (3, 3), 0)
-    cimg = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-    return cimg
+    marked_up_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    return marked_up_image
 
-
-def gradient_create(mosaic_file_path):
+# gradient_create --> create_gradient
+def create_gradient(mosaic_file_path):
     image = cv2.imread(mosaic_file_path, 0)
     sobelx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)  # x
     sobely = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)  # y
-    gradient1 = cv2.subtract(sobelx, sobely)
-    gradient1 = cv2.convertScaleAbs(gradient1)
-    return gradient1
+    gradient_image = cv2.subtract(sobelx, sobely)
+    gradient_image = cv2.convertScaleAbs(gradient_image)
+    return gradient_image
 
 # функция обнаружения кратеров и записи результатов в шейп-файл
-def crater_recognition(gradient1, cimg, shp_name, cv_start_radius = 100, cv_max_radius = 200, cv_param1 = 30, cv_param2 = 25, cv_min_distance = 100):
+def crater_recognition(gradient_image, marked_up_image, shp_name, cv_start_radius = 100, cv_max_radius = 200, cv_param1 = 30, cv_param2 = 25, cv_min_distance = 100):
     # circles = circle_detector.detect(gradient1)
     dtm = gdal.Open(dtm_input) 
     dtm_prj = dtm.GetProjection()
@@ -118,31 +118,31 @@ def crater_recognition(gradient1, cimg, shp_name, cv_start_radius = 100, cv_max_
     dataSource = driver.Open(shp_name, 1)
     crat_layer = dataSource.GetLayer()
 
-    gradient2 = gradient1
+    # gradient2 = gradient1
 
 
 
-    ret, thresh = cv2.threshold(gradient2, 127, 255, 0)
-    # применяем цветовой фильтр
-    # ищем контуры и складируем их в переменную contours
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # hierarchy хранит информацию об иерархии
-    # отображаем контуры поверх изображения
-    image = cv2.drawContours(gradient2, contours, -1, (0, 255, 0), 3)
-    # выводим итоговое изображение в окно
-    cv2.namedWindow('All_con', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('All_con', 600, 600)
-    cv2.imshow('All_con', gradient2)
+    # ret, thresh = cv2.threshold(gradient2, 127, 255, 0)
+    # # применяем цветовой фильтр
+    # # ищем контуры и складируем их в переменную contours
+    # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # # hierarchy хранит информацию об иерархии
+    # # отображаем контуры поверх изображения
+    # image = cv2.drawContours(gradient2, contours, -1, (0, 255, 0), 3)
+    # # выводим итоговое изображение в окно
+    # cv2.namedWindow('All_con', cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow('All_con', 600, 600)
+    # cv2.imshow('All_con', gradient2)
 
-    cv2.namedWindow('grad1', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('grad1', 600, 600)
-    cv2.imshow('grad1', gradient1)
+    # cv2.namedWindow('grad1', cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow('grad1', 600, 600)
+    # cv2.imshow('grad1', gradient1)
 
     # обнаружение кругов и отрисовка их на том же изображении
     crat_id = 0
     radius = cv_start_radius
     while radius < cv_max_radius:
-        circles = cv2.HoughCircles(gradient1, cv2.HOUGH_GRADIENT, 1, cv_min_distance, param1=cv_param1, param2=cv_param2, minRadius=(radius), maxRadius=(radius+11))
+        circles = cv2.HoughCircles(gradient_image, cv2.HOUGH_GRADIENT, 1, cv_min_distance, param1=cv_param1, param2=cv_param2, minRadius=(radius), maxRadius=(radius+11))
         radius += 10
         if circles is None:
             continue
@@ -151,8 +151,8 @@ def crater_recognition(gradient1, cimg, shp_name, cv_start_radius = 100, cv_max_
             # num_i = list(map(float, i))
             # нарисовать окружности
             # print(i)
-            cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+            cv2.circle(marked_up_image, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            cv2.circle(marked_up_image, (i[0], i[1]), 2, (0, 0, 255), 3)
 
             #заносим атрибутивную информацию в слой
             x_coord = geo_info[0] + float(i[0]) * geo_info[1]
@@ -173,10 +173,13 @@ def crater_recognition(gradient1, cimg, shp_name, cv_start_radius = 100, cv_max_
             outFeature = None
             crat_id += 1
     print(crat_id)
-    cv2.imwrite('detected_crat.tif', cimg)
+    # сохраняет получившееся изображение и открывает его 
+    cv2.imwrite('detected_crat.tif', marked_up_image)
     cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('Image', 600, 600)
-    cv2.imshow('Image', cimg)
+    cv2.imshow('Image', marked_up_image)
+    return marked_up_image
+
     # повторная обработка
     # radius = cv_start_radius
     # while radius < cv_max_radius:
@@ -198,17 +201,21 @@ def crater_recognition(gradient1, cimg, shp_name, cv_start_radius = 100, cv_max_
         
 print('end')
     # crater_recognition(gradient_create(create_mosaic_file_path(dtm_input)), image_create(create_mosaic_file_path(dtm_input)))
-    # сохраняет получившееся изображение и открывает его 
 
 
+# def first_button(mosaic):
+
+#     crater_recognition(gradient_create(create_mosaic_file_path(dtm_input)), image_create(create_mosaic_file_path(dtm_input)),  create_shp("crat_circle.shp"))
+
+# if __name__ == "__main__":
+#     first_button()
 if __name__ == "__main__":
     dtm_input = "C:\\projects\\craters_recognition\\GLD100_test.tif"
-    mosaic_file_name = create_mosaic_file_path(dtm_input)
-    create_mosaic(dtm_input, mosaic_file_name)
-    cimg = image_create(mosaic_file_name)
-    gradient = gradient_create(mosaic_file_name)
-    shp_file_name = create_shp("crat_circle.shp")
-    crater_recognition(gradient, cimg, shp_file_name)
+    mosaic_file_name = default_mosaic_filename(dtm_input)
+    mosaic = create_stored_mosaic(dtm_input, mosaic_file_name)
+    create_stored_shp("crat_circle.shp")
+    # first_button(mosaic)
+    crater_recognition(create_gradient(default_mosaic_filename(dtm_input)), get_colorized_image(default_mosaic_filename(dtm_input)),  "crat_circle.shp")
 
 #закрывает все
 cv2.waitKey(0)
